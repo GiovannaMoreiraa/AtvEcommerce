0ec47122ffc6ffafb7434fb5cba19821e7998167
GiovannaMoreiraa <sgiomoreira@edu.unifil.br>
2022-06-25 18:36:52 -0300

    Estilo + alterações back
    Co-authored-by: Arthur Barboza <DevArthurBarboza@users.noreply.github.com>

--- produto.php
+++ produto.php
@@ -5,6 +5,7 @@
     <title>Novo Produto</title>
     <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
     <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="js/script.js"></script>
   </head>
@@ -25,71 +26,80 @@
           
           <div class="row">
 
-          <div class="col-md-6">
-            <label for="exampleFormControlInput1" class="form-label">Nome do Produto:</label>
-          </div>
-          <div class="col-md-6">
-            <input type="text" class="form-control" id="nomeProd" name="nomeProd" placeholder="Digite o Nome do Produto">
-          </div>
+            <div class="col-md-6">
+              <label for="exampleFormControlInput1" class="form-label">Nome do Produto:</label>
+            </div>
+            <div class="col-md-6">
+              <input type="text" class="form-control" id="nomeProd" name="nomeProd" placeholder="Digite o Nome do Produto">
+            </div>
 
-          <div class="col-md-6">
-            <label for="exampleFormControlInput1" class="form-label">Descrição:</label>
-          </div>
-          <div class="col-md-6">
-            <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Breve descrição do Produto">
-          </div>
+            <div class="col-md-6">
+              <label for="exampleFormControlInput1" class="form-label">Descrição:</label>
+            </div>
+            <div class="col-md-6">
+              <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Breve descrição do Produto">
+            </div>
 
-          <div class="col-md-6">
-            <label for="exampleFormControlInput1" class="form-label">Estoque:</label>
-          </div>
-          <div class="col-md-6">
-            <input type="number" class="form-control" id="estoque" name="estoque" placeholder="Quantidade no estoque">
-          </div>
+            <div class="col-md-6">
+              <label for="exampleFormControlInput1" class="form-label">Estoque:</label>
+            </div>
+            <div class="col-md-6">
+              <input type="number" class="form-control" id="estoque" name="estoque" placeholder="Quantidade no estoque">
+            </div>
 
-          <div class="col-md-6">
-            <label for="exampleFormControlInput1" class="form-label">Preço:</label>
-          </div>
-          <div class="col-md-6">
-            <input type="number" class="form-control" id="preco" name="preco" placeholder="Preço do produto">
-          </div>
+            <div class="col-md-6">
+              <label for="exampleFormControlInput1" class="form-label">Preço:</label>
+            </div>
+            <div class="col-md-6">
+              <input type="number" class="form-control" id="preco" name="preco" placeholder="Preço do produto">
+            </div>
 
-          <div class="col-md-6">
-            <label for="exampleFormControlInput1" class="form-label">Marca:</label>
-          </div>
-          <div class="col-md-6" style=" display: flex; justify-content: center;">
-            <select class="form-select" aria-label="Default select example"  >
-              <option selected value="exemplo" disabled>Selecione a marca</option>
-              <!-- fazer um foreach pegando as marcas do banco-->
+            <div class="col-md-6">
+              <label for="exampleFormControlInput1" class="form-label">Marca:</label>
+            </div>
+            <div class="col-md-6" style=" display: flex; justify-content: center;">
+              <select class="form-select" aria-label="Default select example"  >
+                <option selected value="exemplo" disabled>Selecione a marca</option>
+                <!-- fazer um foreach pegando as marcas do banco-->
+                
+              </select>
               
-            </select>
-            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalMarca">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-              </svg>
-            </button>
-            
-          </div>
-
-
-          <div class="col-md-6">
-            <label for="exampleFormControlInput1" class="form-label">Categoria:</label>
-          </div>
-          <div class="col-md-6" style=" display: flex; justify-content: center;">
-            <select class="form-select" aria-label="Default select example">
-              <option selected value="exemplo" disabled>Selecione a categoria</option>
-              <!-- fazer um foreach pegando as categorias do banco-->
+              <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalMarca">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+              </button>
               
-            </select>
-            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalCategoria">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-              </svg>
-            </button>
-          </div>
+            </div>
 
+
+            <div class="col-md-6">
+              <label for="exampleFormControlInput1" class="form-label">Categoria:</label>
+            </div>
+            <div class="col-md-6" style=" display: flex; justify-content: center;">
+              <select class="form-select" aria-label="Default select example">
+                <option selected value="exemplo" disabled>Selecione a categoria</option>
+                <!-- fazer um foreach pegando as categorias do banco-->
+                
+              </select>
+              <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalCategoria">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+              </button>
+            </div>
+
+            <div class="row">
+              <div class="col-md-4"></div>
+              <div class="col-md-4" style="display: grid;">
+                <input href="model/Cliente.class.php?id='.$prod->IDPROD.'" class="btn btn-secondary" type="submit" >
+              </div>
+              <div class="col-md-4"></div>
+            </div>
+          </div>
         </div>
       </div>
-      <input href="model/Cliente.class.php?id='.$prod->IDPROD.'" class="btn btn-outline-secondary" type="submit" >
+     
     </div>
 
     <!-- Modal Marca -->
@@ -113,8 +123,7 @@
 
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-            <button type="button" class="btn btn-primary">Salvar</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Salvar</button>
           </div>
         </div>
       </div>
@@ -139,8 +148,7 @@
               </div>      
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-            <button type="button" class="btn btn-primary">Salvar</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Salvar</button>
           </div>
         </div>
       </div>
